e05bd5a29533e251a23204ab78296b9ba44296ee
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-20 17:02:50 -0300

    Requisitos 7, 8, 9 e 10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-// import Title from './components/Title';
+import Missions from './components/Missions';
+
 class App extends React.Component {
   render() {
     return (
       <main>
         <Header />
         <SolarSystem />
+        <Missions />
       </main>
     );
   }
